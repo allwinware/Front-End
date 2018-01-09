44bd013c821534a7b3c3d3d22199e201736bc639
steve <steve@allwin.bid>
2018-01-09 14:44:28 +0900

    메인 마크업

--- allwinair/resource/js/awa.ui.js
+++ allwinair/resource/js/awa.ui.js
@@ -3,15 +3,65 @@ AWAUI = (function () {
 
     var AWAUI = {
         /**
-         * 공통
+         * 공통 UI
          */
         common: {
             init: function () {
+                AWAUI.common.headerFixed();
+            },
+            headerFixed: function () {
+                //헤더 고정
+                var stickyOffset = $('.awa-header').offset().top;
 
+                $(window).scroll(function () {
+                    var sticky = $('.awa-header'),
+                        scroll = $(window).scrollTop();
+
+                    if (scroll >= stickyOffset) sticky.addClass('fixed');
+                    else sticky.removeClass('fixed');
+                });
             }
         },
         /**
-         * 모달 레이어
+         * 메인 UI
+         */
+        main: {
+            init: function () {
+                AWAUI.main.topBanner();
+                AWAUI.main.matchingSrch();
+                AWAUI.main.matchingSuccess();
+            },
+            //상단 배너
+            topBanner: function () {
+                $('#js-top-banner__close').on('click', function () {
+                    $(this).parent().slideUp('fast');
+                });
+            },
+            matchingSrch: function () {
+                //도착도시
+                $('.ag__link').on('click', function () {
+                    $('.ag__link').removeClass('ag__link--active');
+                    $(this).addClass('ag__link--active');
+                    $('.agc__items').hide();
+                    var activeTab = $(this).attr('href');
+                    $(activeTab).fadeIn();
+                    return false;
+                });
+            },
+            matchingSuccess: function () {
+                var successList = $('#js-matching-success > ul').awaSlide({mode: 'vertical', auto: true});
+                $('#js-matching-success').on('mouseenter', function () {
+                    $('#js-matching-success').addClass('hover');
+                    successList.destroySlider();
+                });
+                $('#js-matching-success').on('mouseleave', function () {
+                    $('#js-matching-success').removeClass('hover');
+                    successList.reloadSlider();
+                });
+            }
+        },
+        /**
+         * 모달 UI
          */
         layers: {
             open: function (modalID) {
@@ -28,4 +78,5 @@ AWAUI = (function () {
 
 $(function () {
     AWAUI.common.init();
+    AWAUI.main.init();
 });
