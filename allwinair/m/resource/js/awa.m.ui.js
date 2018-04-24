62493397bc3a1645efd46e9e31117e6f2adfcb37
steve <steve@allwin.bid>
2018-04-24 11:03:07 +0900

    allwinair mobile

---
+++ allwinair/m/resource/js/awa.m.ui.js
@@ -0,0 +1,31 @@
+AWAUI = (function () {
+    'use strict';
+
+    var AWAUI = {
+        /**
+         * 공통 UI
+         */
+        common: {
+            init: function () {
+
+            }
+        },
+        /**
+         * 모달 UI
+         */
+        layer: {
+            open: function (layerID) {
+
+            },
+            close: function (layerID) {
+
+            }
+        }
+    };
+
+    return AWAUI;
+}());
+
+$(function () {
+    AWAUI.common.init();
+});
