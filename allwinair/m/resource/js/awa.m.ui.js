4fd446eec8a8748f5f419267f0f32d8eaa31e133
steve <steve@allwin.bid>
2018-04-25 14:13:04 +0900

    allwinair mobile

--- allwinair/m/resource/js/awa.m.ui.js
+++ allwinair/m/resource/js/awa.m.ui.js
@@ -11,6 +11,52 @@ AWAUI = (function () {
             }
         },
         /**
+         * 메인 UI
+         */
+        main: {
+            init: function () {
+                var $doc = $(document), $body = $('body'), $matchingSucLayer = $('#js-matching-success__layer');
+
+                var visualSwiper = new Swiper('.visual .swiper-container', {
+                    slidesPerView: 1,
+                    spaceBetween: 15,
+                    centeredSlides: true,
+                    loop: true,
+                    speed: 500,
+                    autoplay: {
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    },
+                    pagination: {
+                        el: '.visual .swiper-pagination',
+                        type: 'progressbar',
+                    },
+                });
+
+                var successSwiper = new Swiper('.matching-success .swiper-container', {
+                    direction: 'vertical',
+                    speed: 1000,
+                    touchRatio: 0,
+                    autoplay: {
+                        delay: 3000,
+                        disableOnInteraction: false,
+                    },
+                });
+
+                $doc.on('click', '#js-matching-success', function () {
+                    $doc.scrollTop(0);
+                    $body.addClass('kill');
+                    $matchingSucLayer.addClass('open');
+
+                });
+
+                $doc.on('click', '#js-layer--hide', function () {
+                    $body.removeClass('kill');
+                    $matchingSucLayer.removeClass('open');
+                });
+            }
+        },
+        /**
          * 모달 UI
          */
         layer: {
