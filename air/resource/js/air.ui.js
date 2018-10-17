5ee4fee9046f35ade678326ddd3ed2f0e62d44cf
steve <steve@allwin.bid>
2018-10-17 14:37:47 +0900

    air 통합

--- air/resource/js/air.ui.js
+++ air/resource/js/air.ui.js
@@ -5,72 +5,130 @@ AIRUI = (function () {
       $customSelect = $('.custom-select select'), //select
       $loadingThumb = $('.wing-thumb'); //로딩 이미지
 
-
-    var AIRUI = {
-        /**
-         * 공통 UI
-         */
-        common: {
-            init: function () {
-              AIRUI.common.select();
-              AIRUI.common.loading();
-            },
-            select: function () {
-              $customSelect.select2({minimumResultsForSearch: -1});
-            },
-          loading: function () {
-            var loading = new TimelineMax({repeat: -1});
-            loading.to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-7C.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-CX.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-LJ.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-NX.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-PR.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-SQ.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-TG.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-VJ.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-VN.svg)'})
-            .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-ZE.svg)'});
-          }
-        },
-      main: {
-        init: function () {
-
-        }
+  var AIRUI = {
+    /**
+     * 공통 UI
+     */
+    common: {
+      init: function () {
+        AIRUI.common.select();
+        AIRUI.common.loading();
+        AIRUI.common.tooltips();
       },
-        /**
-         * 모달 UI
-         */
-        layer: {
-            open: function (layerID) {
-                var targetLayer = "#" + layerID,
-                    dims = $('<div class="dims"></div>');
+      select: function () {
+        $customSelect.select2({minimumResultsForSearch: -1});
+      },
+      loading: function () {
+        var loading = new TimelineMax({repeat: -1});
+        loading.to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-7C.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-CX.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-LJ.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-NX.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-PR.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-SQ.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-TG.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-VJ.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-VN.svg)'})
+        .to($loadingThumb, 0.2, {backgroundImage: 'url(https://cdn.allwin.bid/static/img/wing/wing-ZE.svg)'});
+      },
+      tooltips: function () {
+        $('.js-tooltips').each(function () {
+          var position, direction = $(this).data('tooltip-direction');
+          switch (direction) {
+            case 'top':
+              position = {
+                my: 'center bottom', at: 'center top', collision: 'none',
+                using: function (position, feedback) {
+                  $(this).addClass(feedback.vertical).css(position);
+                }
+              };
+              break;
+            case 'bottom':
+              position = {
+                my: 'center top', at: 'center bottom', collision: 'none',
+                using: function (position, feedback) {
+                  $(this).addClass(feedback.vertical).css(position);
+                }
+              };
+              break;
+            case 'left':
+              position = {
+                my: 'right center', at: 'left center',
+                using: function (position, feedback) {
+                  $(this).addClass(feedback.horizontal).css(position);
+                }
+              };
+              break;
+            case 'right':
+              position = {
+                my: 'left center', at: 'right center',
+                using: function (position, feedback) {
+                  $(this).addClass(feedback.horizontal).css(position);
+                }
+              };
+              break;
+          }
 
-                $('body').addClass('kill');
-                $(targetLayer).css({
-                    'top': Math.max(0, (($(window).height() - $(targetLayer).height()) / 2) + $(window).scrollTop()) + "px",
-                    'left': Math.max(0, (($(window).width() - $(targetLayer).width()) / 2) + $(window).scrollLeft()) + "px"
-                }).fadeIn('fast');
-                $(dims).appendTo('body').css({
-                    'width': '100%',
-                    'height': '100%',
-                    'display': 'none',
-                    'background-color': '#000',
-                    'filter': 'alpha(opacity=50)',
-                    'position': 'fixed',
-                    'top': 0,
-                    'left': 0,
-                    'z-index': 99
-                }).fadeTo('fast', 0.5);
+          $(this).tooltip({
+            show: null,
+            items: "[data-tooltip-txt]",
+            position: position,
+            content: function () {
+              return $(this).data("tooltip-txt");
             },
-            close: function (layerID) {
-                $('body').removeClass('kill');
-                $("#" + layerID).fadeOut('fast');
-                $('.dims').remove();
+            open: function (event, ui) {
+              if (direction == "right" || direction == "left") {
+                ui.tooltip.animate({left: ui.tooltip.position().left + 5}, "fast");
+              } else if (direction == "top") {
+                ui.tooltip.animate({top: ui.tooltip.position().top - 5}, "fast");
+              } else if (direction == "bottom") {
+                ui.tooltip.animate({top: ui.tooltip.position().top + 5}, "fast");
+              }
             }
-        }
-    };
+          });
+        });
+        $(".ui-helper-hidden-accessible").remove();
+      }
+    },
+    main: {
+      init: function () {
 
-    return AIRUI;
+      }
+    },
+    /**
+     * 모달 UI
+     */
+    layer: {
+      open: function (layerID) {
+        var targetLayer = "#" + layerID,
+            dims = $('<div class="dims"></div>');
+
+        $('body').addClass('kill');
+        $(targetLayer).css({
+          'top': Math.max(0, (($(window).height() - $(targetLayer).height()) / 2) + $(window).scrollTop()) + "px",
+          'left': Math.max(0, (($(window).width() - $(targetLayer).width()) / 2) + $(window).scrollLeft()) + "px"
+        }).fadeIn('fast');
+        $(dims).appendTo('body').css({
+          'width': '100%',
+          'height': '100%',
+          'display': 'none',
+          'background-color': '#000',
+          'filter': 'alpha(opacity=50)',
+          'position': 'fixed',
+          'top': 0,
+          'left': 0,
+          'z-index': 99
+        }).fadeTo('fast', 0.5);
+      },
+      close: function (layerID) {
+        $('body').removeClass('kill');
+        $("#" + layerID).fadeOut('fast');
+        $('.dims').remove();
+      }
+    }
+  };
+
+  return AIRUI;
 }());
 
 $(function () {
