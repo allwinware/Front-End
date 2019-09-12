df010f24130a07cc9afe0ea537e77c96e24bdd44
hejilyallwin <54651216+hejilyallwin@users.noreply.github.com>
2019-09-13 03:44:15 +0900

    Add files via upload

--- ags/html/interpark/js/common.js
+++ ags/html/interpark/js/common.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
-
+	$(".seatWrap ul li").click(function(){
+		$(this).toggleClass("selected");
+	});
+	$(".passengerWrap ul li").click(function(){
+		$(this).siblings().removeClass("selected");
+		$(this).addClass("selected");
+	});
+	$("a.qa").hover(function(){
+		$(this).next().show();
+	}, function(){
+		$(this).next().hide();
+	});
 });
 
 
