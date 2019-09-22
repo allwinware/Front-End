a24faebbd6dc802dda38fcb6204803096f902053
hejilyallwin <54651216+hejilyallwin@users.noreply.github.com>
2019-09-23 08:47:19 +0900

    Add files via upload

--- ags/html/interpark/js/common.js
+++ ags/html/interpark/js/common.js
@@ -18,6 +18,23 @@ $(document).ready(function () {
 	$(".meal .passengerWrap ul li").click(function(){
 		$(this).toggleClass("selected");
 	});
+
+	$(document).on('click', '.popupBtn', function() {
+		var popupIndex = $(this).attr('id');
+		$(".popupContainer."+popupIndex).show();
+		var popupwidth = $('.popupContainer .popupWrap').not( ":hidden" ).width();
+		var popupHeight = $('.popupContainer .popupWrap').not( ":hidden" ).height();
+		$('.popupContainer .popupWrap').not( ":hidden" ).css("left","calc(50% - "+(popupwidth+2)/2+"px");
+		$('.popupContainer .popupWrap').not( ":hidden" ).css("top","calc(50% - "+(popupHeight+2)/2+"px");
+	});
+
+	$(document).on('click', '.popupDimmed', function() {
+		$(this).parent().hide();
+	});
+	$(document).on('click', '.popupContainer .close', function() {
+		$('.popupContainer').hide();
+	});
+
 });
 
 
