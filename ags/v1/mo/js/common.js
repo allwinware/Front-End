497c09a1e1ec4e451618a38a826d53be0e04a9ed
allwin <ayul@allwin.bid>
2021-01-07 17:42:40 +0900

    퍼블리싱 파일 전체 정리(노이사님 지시사항)

---
+++ ags/v1/mo/js/common.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+	$(".pageWrap.wrapper").css("height", $(window).height() );
+
+	$(".passenger ul li").click(function(){
+		$(this).siblings().removeClass("selected");
+		$(this).addClass("selected");
+	});
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > 234 ) {
+			$(".pageWrap").addClass("passengerFixed");
+		}	else	{
+			$(".pageWrap").removeClass("passengerFixed");
+		}
+	});
+	$(".paySummaryContainer .moreBtn").click(function(){
+		$(".paySummaryContainer .detailWrap").css("height", $(window).height() - 100 );
+		$(".paySummaryContainer").toggleClass("open");
+		$(".paySummaryContainer").toggleClass("close");
+		$("body").toggleClass("menuOpen");
+		$(".paySummaryContainer.open .paySummaryWrap").css("height", $(window).height() - 44 );
+		$(".paySummaryContainer.close .paySummaryWrap").css("height", 56 );
+	});
+
+	$(document).on('click', '.popupBtn', function() {
+		var popupIndex = $(this).attr('id');
+		$(".popupContainer."+popupIndex).show();
+		var popupHeight = $('.popupContainer .popupWrap').not( ":hidden" ).height();
+		$('.popupContainer .popupWrap').not( ":hidden" ).css("top","calc(50% - "+(popupHeight+2)/2+"px");
+	});
+/*
+	$(document).on('click', '.popupDimmed', function() {
+		$(this).parent().hide();
+	});
+*/
+	$(document).on('click', '.popupContainer .close', function() {
+		$('.popupContainer').hide();
+	});
+//	191119
+	$(document).on('click', '.alertBtn', function() {
+		var alertIndex = $(this).attr('id');
+		$(".alertContainer."+alertIndex).show();
+		$("body").addClass("menuOpen");
+	});
+	$(document).on('click', '.alertContainer .close', function() {
+		$('.alertContainer').hide();
+		$("body").removeClass("menuOpen");
+	});
+//	- 191119
+
+	$(".contentContainer.pay .agreeWrap ul li .more").click(function(){
+		$(this).parent().toggleClass("open");
+	});
+
+	$(".contentContainer.pay .choiceWrap .btn").click(function(){
+		$(".payDetailContainer").show();
+	});
+	$(".payDetailContainer .top a.close").click(function(){
+		$(".payDetailContainer").hide();
+	});
+
+    // 191205 navigation menu 추가
+    $(document).on('click', '.btn.nav', function(){
+        $(".navigation-layer").addClass("active");
+    });
+    $(document).on('click', '.layer-close', function(){
+        $(".navigation-layer").removeClass("active");
+    });
+});
+
---
+++ ags/v1/pc/7c/js/common.js
@@ -0,0 +1,187 @@
+/* 상품가격 등의 숫자정보 표현 시 콤마 추가기능 정의 */
+$.fn.digits = function(){
+    return this.each(function(){
+        $(this).text( $(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") );
+    })
+};
+$(document).ready(function () {
+    /* 상품가격 등의 숫자정보 표현 시 콤마 추가 */
+    (function(digitsHandler){
+        $(".digits").digits();
+    })();
+
+	$(".seatWrap ul li").click(function(){
+		$(this).toggleClass("selected");
+	});
+	$(".seat .passengerWrap ul li").click(function(){
+		$(this).siblings().removeClass("selected");
+		$(this).addClass("selected");
+	});
+	$("a.qa").hover(function(){
+		$(this).next().show();
+	}, function(){
+		$(this).next().hide();
+	});
+	$(".baggage .passengerWrap > ul > li").click(function(){
+		$(this).siblings().removeClass("selected");
+		$(this).addClass("selected");
+	});
+	$(".meal .passengerWrap ul li").click(function(){
+		$(this).toggleClass("selected");
+	});
+
+    (function(popupHandler){
+
+        /*스크롤바 너비를 구합니다.*/
+        function getScrollBarWidth() {
+            var inner = document.createElement('p');
+            inner.style.width = "100%";
+            inner.style.height = "200px";
+
+            var outer = document.createElement('div');
+            outer.style.position = "absolute";
+            outer.style.top = "0px";
+            outer.style.left = "0px";
+            outer.style.visibility = "hidden";
+            outer.style.width = "200px";
+            outer.style.height = "150px";
+            outer.style.overflow = "hidden";
+            outer.appendChild (inner);
+
+            document.body.appendChild (outer);
+            var w1 = inner.offsetWidth;
+            outer.style.overflow = 'scroll';
+            var w2 = inner.offsetWidth;
+            if (w1 === w2) w2 = outer.clientWidth;
+
+            document.body.removeChild (outer);
+
+            return (w1 - w2);
+        };
+
+        /*윈도우 사이즈에 따라 팝업을 포지셔닝합니다.*/
+        function calcPopupPosition(target){
+            if(target.length > 0){
+                var popupHeight = target.height(),
+                    popupWidth = target.width();
+                popupIndex = target.attr('id');
+                if(popupHeight > $(window).height()){
+                    target.css("top","0");
+                    target.siblings(".popupDimmed").css({
+                        "right" : getScrollBarWidth()
+                    });
+                } else {
+                    target.css("top","calc(50% - "+(popupHeight+2)/2+"px");
+                    target.siblings(".popupDimmed").css({
+                        "right" : 0
+                    });
+                }
+                if(popupWidth > $(window).width()){
+                    target.css("left","0");
+                    target.siblings(".popupDimmed").css({
+                        "bottom" : getScrollBarWidth()
+                    });
+                } else {
+                    target.css("left","calc(50% - "+(popupWidth+2)/2+"px");
+                    target.siblings(".popupDimmed").css({
+                        "bottom" : 0
+                    });
+
+                }
+            }
+        }
+
+        /*팝업 동작 정의*/
+        $(document).on('click', '.popupBtn', function() {
+            var popupIndex = $(this).attr('id');
+            $(".popupContainer."+popupIndex).show();
+            var $popup = $('.popupContainer .popupWrap').not( ":hidden" );
+            var popupwidth = $popup.width();
+            $("html,body").css("overflow", "hidden");
+            calcPopupPosition($popup)
+        });
+
+        /*팝업 포지셔닝 변경을 위해 리사이즈 이벤트 설정*/
+        $(window).on("resize", function(){
+            calcPopupPosition($('.popupContainer .popupWrap').not(":hidden"))
+        }).resize();
+    })();
+
+	$(document).on('click', '.agreeWrap .tabSelect li', function() {
+		var tabIndex = $(this).attr('id');
+		$(this).siblings().removeClass("on");
+		$(this).addClass("on");
+		$(".agreeWrap .tabContent li").removeClass("on");
+		$(".agreeWrap .tabContent li."+tabIndex).addClass("on");
+	});
+
+    $(".anchorWrap a").click(function(event){    
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+
+	$(document).on('click', '.popupDimmed:not(.preventClick)', function() {/*200212 dim 클릭 시 자동으로 닫히지 않게 처리하는 특수 클래스를 위한 처리 추가*/
+		$(this).parent().hide();
+        $("html,body").css("overflow", "auto");
+	});
+	$(document).on('click', '.popupContainer .close', function() {
+		$('.popupContainer').hide();
+        $("html,body").css("overflow", "auto");
+	});
+
+	$(".popupContainer.buyHistory .popupContent > ul > li > p").click(function(){
+		$(this).parents().toggleClass("on");
+	});
+
+	$(".selectForm > p").click(function(){
+		$(this).toggleClass("on");
+	});
+	$(".selectForm > ul > li").click(function(){
+		$(".selectForm > p").removeClass("on");
+	});
+
+
+    $("tr.titleWrap").click(function(event){    
+		$(this).siblings().removeClass("view");
+		$(this).toggleClass("view");
+    });
+
+
+
+
+
+
+
+
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > 302 ) {
+			$(".priceContainer").addClass("menuFixed");
+		}	else	{
+			$(".priceContainer").removeClass("menuFixed");
+		}
+	});	
+
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > 80 ) {
+			$(".headerContainer.main").addClass("fixed");
+		}	else	{
+			$(".headerContainer.main").removeClass("fixed");
+		}
+	});	
+
+
+	/*$(window).scroll(function() {
+		var scrollHeight = $(document).height();
+		var scrollPosition = $(window).height() + $(window).scrollTop();		
+		if (scrollPosition > scrollHeight - 284 + 948 - $(".priceContainer").innerHeight()) {		
+				$(".priceContainer").addClass("bottom");
+				$(".priceContainer").css("bottom",scrollPosition - scrollHeight + 284);
+			}	else	{
+				$(".priceContainer").removeClass("bottom");
+				$(".priceContainer").css("bottom","auto");
+			}
+		});	*/
+
+});
+
+
