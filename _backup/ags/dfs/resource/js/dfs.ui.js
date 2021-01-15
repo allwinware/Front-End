a01781657a3e7b4146c2b66887e6267b643faa78
allwin <ayul@allwin.bid>
2021-01-15 15:38:34 +0900

    [AGS PC v1.0] 메인페이지 상품 갯수에 상관없이 나열될 수 있도록 수정

---
+++ _backup/ags/dfs/resource/js/dfs.ui.js
@@ -0,0 +1,68 @@
+/* 상품가격 등의 숫자정보 표현 시 콤마 추가기능 정의 */
+$.fn.digits = function(){
+    return this.each(function(){
+        $(this).text( $(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") );
+    })
+};
+
+$(document).ready(function(){
+    var $body = $("body"),
+        $page = $("#dfs-wrap"),
+        $pageHeader = $("#header");
+
+    /* 상품가격 등의 숫자정보 표현 시 콤마 추가 */
+    (function(digitsHandler){
+        $(".digits").digits();
+    })();
+
+    /* 맨 위로 버튼 동작 정의 */
+    (function(){
+        var $btn_scrollTop = $("#btn_scroll-top");
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $btn_scrollTop.fadeIn();
+            } else {
+                $btn_scrollTop.fadeOut();
+            }
+        });
+        $btn_scrollTop.click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 600);
+            return false;
+        });
+
+    })();
+
+    /* 부드럽게 스크롤 되는 페이지 내부 링크 정의 */
+    (function(){
+        $(document).on('click', 'a[data-type="scroll-link"]', function (event) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: $($.attr(this, 'href')).offset().top - 80
+            }, 1200);
+        });
+    })();
+
+    /* 브라우저 상단에 붙어있는 Global Header 동작 정의  */
+    (function(stickyHeader){
+        function compScroll(el, current, setPoint){
+            if (current > setPoint) {
+                el.addClass("scroll-active");
+            } else {
+                el.removeClass("scroll-active")
+            }
+        }
+        function headerHandler(){
+            var currentScrollTop = $(document).scrollTop();
+            if($page.attr("data-page-type") === "main"){
+                var $visual = $("#visual"),
+                    mainSetPoint = $visual.offset().top + $visual.height() - $("#dfs-header").height();
+                compScroll($page, currentScrollTop, mainSetPoint);
+            } else {
+                compScroll($page, currentScrollTop, 0);
+            }
+        }
+        $(window).on("scroll", function(){
+            headerHandler();
+        }).scroll()
+    })();
+});
