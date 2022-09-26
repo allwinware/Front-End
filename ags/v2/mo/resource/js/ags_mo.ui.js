5168a78b7aef9e6f2137dde6e934ab675128883e
hjy224 <hjy224@allwin.co.kr>
2022-09-26 09:11:50 +0900

    [AGS MO] 카드 할부 기능 TEST

--- ags/v2/mo/resource/js/ags_mo.ui.js
+++ ags/v2/mo/resource/js/ags_mo.ui.js
@@ -469,3 +469,74 @@ $(window).on("load", function () {
 
     })
 });
+    /* selectric */
+    (function () {
+        if ($(".selectric-select-wrap").length > 0) {
+            $(".selectric-select-wrap select").each(function () {
+                $(this).selectric()
+            })
+        }
+    })();
+/*스크롤바 기본 옵션*/
+var scrollOption = {
+    setWidth: false,
+    setHeight: false,
+    setTop: 0,
+    setLeft: 0,
+    axis: "y",
+    scrollbarPosition: "inside",
+    scrollInertia: 200,
+    autoDraggerLength: true,
+    autoHideScrollbar: true,
+    autoExpandScrollbar: false,
+    alwaysShowScrollbar: false,
+    snapAmount: null,
+    snapOffset: 0,
+    mouseWheel: {
+        enable: true,
+        scrollAmount: "auto",
+        axis: "y",
+        preventDefault: false,
+        deltaFactor: "auto",
+        normalizeDelta: false,
+        invert: false,
+        disableOver: ["select", "option", "keygen", "datalist", "textarea"]
+    },
+    scrollButtons: {
+        enable: false,
+        scrollType: "stepless",
+        scrollAmount: "auto"
+    },
+    keyboard: {
+        enable: true,
+        scrollType: "stepless",
+        scrollAmount: "auto"
+    },
+    contentTouchScroll: 25,
+    advanced: {
+        autoExpandHorizontalScroll: false,
+        autoScrollOnFocus: "input,textarea,select,button,datalist,keygen,a[tabindex],area,object,[contenteditable='true']",
+        updateOnContentResize: true,
+        updateOnImageLoad: true,
+        updateOnSelectorChange: false,
+        releaseDraggableSelectors: false
+    },
+    theme: "light",
+    callbacks: {
+        onInit: false,
+        onScrollStart: false,
+        onScroll: false,
+        onTotalScroll: false,
+        onTotalScrollBack: false,
+        whileScrolling: false,
+        onTotalScrollOffset: 0,
+        onTotalScrollBackOffset: 0,
+        alwaysTriggerOffsets: true,
+        onOverflowY: false,
+        onOverflowX: false,
+        onOverflowYNone: false,
+        onOverflowXNone: false
+    },
+    live: false,
+    liveSelector: null
+}
