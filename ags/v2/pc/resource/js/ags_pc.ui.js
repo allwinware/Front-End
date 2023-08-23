c143de5b3b56d2e7ba453b8570c385e4c9bfd902
hjy224 <hjy224@allwin.co.kr>
2023-08-23 14:15:05 +0900

    [ALPHA PC] cvc 추가 및 결제 자리이동 추가

--- ags/v2/pc/resource/js/ags_pc.ui.js
+++ ags/v2/pc/resource/js/ags_pc.ui.js
@@ -518,3 +518,85 @@ $(window).on("load", function () {
         });
     })();
 });
+
+
+// 레이어팝업
+function layerPop(id) {
+	var $el = $('#' + id);
+
+	if ($el.hasClass('alert') || $el.hasClass('sns') || $el.hasClass('keypad') || $el.hasClass('tooltip') || $el.hasClass('menuAll')) {
+		// dimmed 개수 확인 후 추가
+		if ($el.find('> .dimmed').length == 0) {
+			// alert, bottom 팝업일 경우 dimmed 추가
+			$el.prepend('<div class="dimmed" />');
+		}
+	}
+
+	// PIST2.0 추가 - 21.04.23
+	if ($el.hasClass('pist20')) {
+		layerPop20(id);
+		return false;
+	}
+
+	// wrapper 스크롤 hidden 처리
+	$('#wrapper').css('overflow-y', 'hidden');
+
+	// 팝업 표시
+	$el.fadeIn();
+
+	if ($el.hasClass('menuAll')) {
+		$('.menuAll').find('.wrapper_popup').stop(true, true).animate({
+			right: '0'
+		}, 400);
+	}
+
+	// 닫기버튼 클릭시 레이어 닫힘
+	$el.find('.btn_close_popup,.btn_close_popup_x, .wrapper_popup .pop_close, .btn_close_tooltip').off('click').on('click', function () {
+		closeLayerPop();
+	});
+
+	if ($el.hasClass('sns') || $el.hasClass('tooltip') || $el.hasClass('clickable_dim') || $el.hasClass('menuAll')) {
+		// 딤드 배경 클릭시 레이어 닫힘
+		$el.find('.dimmed').click(function () {
+			closeLayerPop();
+		});
+	}
+
+	function closeLayerPop() {
+		if ($el.hasClass('menuAll')) {
+			$el.find('.wrapper_popup').animate({
+				right: -$('.menuAll').width()
+			}, 400, function () {
+				$el.find('.dimmed').remove();
+				$el.fadeOut();
+			});
+		} else {
+			$el.fadeOut(300, function () {
+				// dimmed 삭제
+				if ($el.hasClass('sns') || $el.hasClass('keypad') || $el.hasClass('tooltip') || $el.hasClass('clickable_dim')) {
+					$el.find('.dimmed').remove();
+				}
+
+			});
+		}
+		// wrapper 스크롤 hidden 처리 해제
+		$('#wrapper').css('overflow-y', 'auto');
+		return false;
+	}
+
+	// 탭이 있을 경우 탭 상단좌표 가져오기
+	$el.find('.grp_tab.st_01').each(function () {
+		var tabTopPopup = $(this).position().top - 52;
+		initTab(tabTopPopup);
+	});
+}
+/*좌석선택 오렌지*/
+
+$(document).on("click", ".seat_btn", function () {
+    var isActive	= $(this).hasClass("seat_active");
+    if (isActive) {
+        $(this).removeClass("seat_active").find("span").show();
+    } else {
+        $(this).addClass("seat_active").find("span").hide();;
+    }
+});
