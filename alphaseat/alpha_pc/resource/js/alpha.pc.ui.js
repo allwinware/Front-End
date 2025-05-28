83819375f32be6e58692b07ec0b80f250768e639
hjy224 <hjy224@allwin.co.kr>
2025-05-28 09:01:39 +0900

    alpha-pc 0528-1

--- alphaseat/alpha_pc/resource/js/alpha.pc.ui.js
+++ alphaseat/alpha_pc/resource/js/alpha.pc.ui.js
@@ -614,8 +614,8 @@ function layerPop(id) {
 
 	// 닫기버튼 클릭시 레이어 닫힘
 	$el.find('.btn_close_popup,.btn_close_popup_x, .btn_close_popup1, .wrapper_popup .pop_close, .btn_close_tooltip').off('click').on('click', function () {
-		closeLayerPop();
-        $(".dimmed_bg").fadeOut();
+		closeLayerPop();       
+        $("html").css("overflow", "unset");
 	});
 
 	if ($el.hasClass('sns') || $el.hasClass('tooltip') || $el.hasClass('clickable_dim') || $el.hasClass('menuAll')) {
@@ -690,9 +690,14 @@ $(document).ready(function() {
     });
 
     $('.btn_bd_md').on('click', function() {
-        $('.dimmed_bg').fadeIn();       
+        $("html").css("overflow", "hidden");    
     });
+  
+  
   });
+
+
+
 
 
 
